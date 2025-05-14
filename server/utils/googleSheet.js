424b04d228894fe1564dcc8ca4a2cd11d78c0dd5
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-05-14 13:25:05 +0530

    ..

--- server/utils/googleSheet.js
+++ server/utils/googleSheet.js
@@ -16,26 +16,27 @@ const keys = {
 };
 
 
+const { google } = require('googleapis');
+
 const auth = new google.auth.GoogleAuth({
   credentials: keys,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-const SHEET_ID = process.env.GOOGLE_SHEET_ID;
- // Add this line temporarily
-
 async function appendToSheet(dataArray) {
   const client = await auth.getClient();
   const sheets = google.sheets({ version: 'v4', auth: client });
 
-  await sheets.spreadsheets.values.append({
+  const response = await sheets.spreadsheets.values.append({
     spreadsheetId: SHEET_ID,
     range: 'Sheet1!A1',
     valueInputOption: 'USER_ENTERED',
     resource: {
-        values: [dataArray],
+      values: [dataArray],
     },
-});
+  });
+
+  return response;
 }
 
 module.exports = appendToSheet;
