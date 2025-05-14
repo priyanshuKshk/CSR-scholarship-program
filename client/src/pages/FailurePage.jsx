45a63a347a027f7959594c6b9b7f9510b9d207f9
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-05-14 12:44:10 +0530

    ..

--- client/src/pages/FailurePage.jsx
+++ client/src/pages/FailurePage.jsx
@@ -6,7 +6,7 @@ export default function FailurePage() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-red-50">
       <h1 className="text-3xl font-bold text-red-700 mb-4">❌ Submission Failed</h1>
       <p className="text-lg mb-6">Something went wrong while submitting your application.</p>
-      <Link to="/" className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition">
+      <Link to="/" className="bg-blue-900 text-white px-4 py-2 rounded hover:bg-red-700 transition">
         Try Again / Go Back to Home
       </Link>
     </div>
